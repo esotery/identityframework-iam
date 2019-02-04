8817b3dad1a8eb80fac3b57601d67bddf433a79e
Jan Czopik <jan.czopik@bohemian.studio>
2019-02-04 16:13:45 +0100

    Added possibility to have proxy context

--- lib/IdentityFramework.Iam/IdentityFramework.Iam.Ef/Extensions.cs
+++ lib/IdentityFramework.Iam/IdentityFramework.Iam.Ef/Extensions.cs
@@ -28,8 +28,27 @@ namespace IdentityFramework.Iam.Ef
             where TRole : IdentityRole<TKey>
             where TKey : IEquatable<TKey>
         {
+            AddIamEntityFramework<IamDbContext<TUser, TRole, TKey>, TUser, TRole, TKey>(services, optionsBuilder, configure);
+        }
+
+        /// <summary>
+        /// Adds the IAM entity framework.
+        /// </summary>
+        /// <typeparam name="TProxyContext">The context derived from IamDbContext which will be resolved in DI</typeparam>
+        /// <typeparam name="TUser">The type of the user.</typeparam>
+        /// <typeparam name="TRole">The type of the role.</typeparam>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <param name="services">The services.</param>
+        /// <param name="optionsBuilder">The options builder.</param>
+        /// <param name="configure">The configure.</param>
+        public static void AddIamEntityFramework<TProxyContext, TUser, TRole, TKey>(this IServiceCollection services, Action<DbContextOptionsBuilder> optionsBuilder, Action<IamOptions> configure = null)
+            where TProxyContext : IamDbContext<TUser, TRole, TKey>
+            where TUser : IdentityUser<TKey>
+            where TRole : IdentityRole<TKey>
+            where TKey : IEquatable<TKey>
+        {
             services.AddIamCore(configure);
-            services.AddDbContext<IamDbContext<TUser, TRole, TKey>>(optionsBuilder);
+            services.AddDbContext<IamDbContext<TUser, TRole, TKey>, TProxyContext>(optionsBuilder);
             services.AddSingleton(typeof(IIamProvider), typeof(IamProvider<TUser, TRole, TKey>));
         }
 
@@ -46,6 +65,27 @@ namespace IdentityFramework.Iam.Ef
         public static void AddMultiTenantIamEntifyFramework<TUser, TRole, TKey, TTenantKey>(this IServiceCollection services, Action<DbContextOptionsBuilder> optionsBuilder, Action<IamMultiTenantOptions> configure = null) 
             where TUser : IdentityUser<TKey> 
             where TRole : IdentityRole<TKey> 
+            where TKey : IEquatable<TKey>
+            where TTenantKey : IEquatable<TTenantKey>
+        {
+            AddMultiTenantIamEntifyFramework<MultiTenantIamDbContext<TUser, TRole, TKey, TTenantKey>, TUser, TRole, TKey, TTenantKey>(services, optionsBuilder, configure);
+        }
+
+        /// <summary>
+        /// Adds the multi tenant IAM entify framework.
+        /// </summary>
+        /// <typeparam name="TProxyContext">The context derived from IamDbContext which will be resolved in DI</typeparam>
+        /// <typeparam name="TUser">The type of the user.</typeparam>
+        /// <typeparam name="TRole">The type of the role.</typeparam>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <typeparam name="TTenantKey">The type of the tenant key.</typeparam>
+        /// <param name="services">The services.</param>
+        /// <param name="optionsBuilder">The options builder.</param>
+        /// <param name="configure">The configure.</param>
+        public static void AddMultiTenantIamEntifyFramework<TProxyContext, TUser, TRole, TKey, TTenantKey>(this IServiceCollection services, Action<DbContextOptionsBuilder> optionsBuilder, Action<IamMultiTenantOptions> configure = null)
+            where TProxyContext : MultiTenantIamDbContext<TUser, TRole, TKey, TTenantKey>
+            where TUser : IdentityUser<TKey>
+            where TRole : IdentityRole<TKey>
             where TKey : IEquatable<TKey>
             where TTenantKey : IEquatable<TTenantKey>
         {
